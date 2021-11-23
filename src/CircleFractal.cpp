43b529a8909347b6eb1123ef97c93865865bf600
Grammi1120 <gradmar.maldonado@upr.edu>
2021-11-22 21:40:25 -0400

    Fixed Issue with Circle Fractal. Changed Circle.

--- src/CircleFractal.cpp
+++ src/CircleFractal.cpp
@@ -2,8 +2,8 @@
 
 CircleFractal::CircleFractal()
 {
-    x=ofGetWidth();
-    y=ofGetHeight();
+    x=ofGetWidth()/2;
+    y=ofGetHeight()/2;
     n=4;
 }
 void CircleFractal::draw()
@@ -11,8 +11,9 @@ void CircleFractal::draw()
     pinta(x, y, n);
 }
 void CircleFractal::pinta(int x, int y, int n){
+    if(n==0){ return; }
     ofSetColor(color[n]);
-    ofDrawCircle(x, y, 100);
+    ofDrawEllipse(x, y, ofGetWidth()/16, ofGetHeight()/4);
     pinta(x + 100, y, n - 1);
     pinta(x - 100, y, n - 1);
     pinta(x, y + 100, n - 1);
